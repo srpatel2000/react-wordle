7f40f95983f04127fc49c8ae363e1f3bc6164e84
srpatel2000 <49801632+srpatel2000@users.noreply.github.com>
2022-03-01 14:05:13 -0800

    up to error with win/loss message

--- src/components/modals/StatsModal.tsx
+++ src/components/modals/StatsModal.tsx
@@ -10,6 +10,8 @@ import {
   GUESS_DISTRIBUTION_TEXT,
   NEW_WORD_TEXT,
   SHARE_TEXT,
+  CORRECT_WORD_MESSAGE,
+  WIN_MESSAGES
 } from '../../constants/strings'
 
 type Props = {
@@ -48,45 +50,26 @@ export const StatsModal = ({
       </BaseModal>
     )
   }
-  return (
-    <BaseModal
-      title={STATISTICS_TITLE}
-      isOpen={isOpen}
-      handleClose={handleClose}
-    >
-      <StatBar gameStats={gameStats} />
-      <h4 className="text-lg leading-6 font-medium text-gray-900 dark:text-gray-100">
-        {GUESS_DISTRIBUTION_TEXT}
-      </h4>
-      <Histogram gameStats={gameStats} />
-      {(isGameLost || isGameWon) && (
-        <div className="mt-5 sm:mt-6 columns-2 dark:text-white">
-          <div>
-            <h5>{NEW_WORD_TEXT}</h5>
-            <Countdown
-              className="text-lg font-medium text-gray-900 dark:text-gray-100"
-              date={tomorrow}
-              daysInHours={true}
-            />
-          </div>
-          <button
-            type="button"
-            className="mt-2 w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
-            onClick={() => {
-              shareStatus(
-                guesses,
-                isGameLost,
-                isHardMode,
-                isDarkMode,
-                isHighContrastMode
-              )
-              handleShare()
-            }}
-          >
-            {SHARE_TEXT}
-          </button>
-        </div>
-      )}
-    </BaseModal>
-  )
+
+  if (isGameLost) {
+    return (
+      <BaseModal
+        title={CORRECT_WORD_MESSAGE}
+        isOpen={isOpen}
+        handleClose={handleClose}
+      >
+      </BaseModal>
+    )
+  }
+  
+  if (isGameWon) {
+    return (
+      <BaseModal
+        title={WIN_MESSAGES}
+        isOpen={isOpen}
+        handleClose={handleClose}
+      >
+      </BaseModal>
+    )  
+  }
 }
--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -1,13 +1,13 @@
-export const GAME_TITLE = process.env.REACT_APP_GAME_NAME!
+export const GAME_TITLE = 'Wordle: Warren Res Life Edition'
 
-export const WIN_MESSAGES = ['Great Job!', 'Awesome', 'Well done!']
+export const WIN_MESSAGES = 'Great Job! Jisoo, the rocks box will be in the staff office for you to pick up!'
 export const GAME_COPIED_MESSAGE = 'Game copied to clipboard'
 export const NOT_ENOUGH_LETTERS_MESSAGE = 'Not enough letters'
 export const WORD_NOT_FOUND_MESSAGE = 'Word not found'
 export const HARD_MODE_ALERT_MESSAGE =
   'Hard Mode can only be enabled at the start!'
 export const CORRECT_WORD_MESSAGE = (solution: string) =>
-  `The word was ${solution}`
+  `Sorry you didn't get it! The person who got the rocks box was ${solution}. It'll be in the staff office for you to pick up!`
 export const WRONG_SPOT_MESSAGE = (guess: string, position: number) =>
   `Must use ${guess} in position ${position}`
 export const NOT_CONTAINED_MESSAGE = (letter: string) =>
